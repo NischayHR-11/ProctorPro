f7e1a4c3f6950bf1b4d6027c098aa3c0c5a1c9d0
NischayHR-11 <nischayhr11@gmail.com>
2024-11-06 21:22:19 +0530

    Intialized With Basic requirements of The Apllication

---
+++ index.js
@@ -0,0 +1,38 @@
+const express =require("express");
+const app = express();
+const port=8080;
+const mongoose =require("mongoose");
+const method=require("method-override");                                // Form Contains Only two methods(get,post) ,but to make it convertable this package is reuired.
+const Expresserror=require("./ExpressUserDefinedError")
+const path=require("path");                                             // For Setting Deafult Paths
+
+
+app.set("view engine","ejs");                                    // When The Response Is 'Rendered' default path to access.
+app.set("views",path.join(__dirname,"/views"));               
+app.use(express.static(path.join(__dirname,"/public")));         // Default middleware : for default paths.
+app.use(express.urlencoded({extended:true}));                    // Default middleware : for get data sent from the request.
+app.use(method('_method'));
+
+app.listen(port,(req,res)=>{
+
+    console.log("server Started..");
+});
+
+app.get("/",(req,res)=>{
+
+    res.send("working..");
+});
+
+app.all("*",(req,res,next)=>{
+
+    next(new Expresserror(404,"Page Not Found !!"));
+});
+
+app.use((err,req,res,next)=>{                                   // Error Handling MiddleWare.
+
+    let{status=500,message="Something went wrong"}=err;
+    res.status(status).send(message);
+});
+
+
+
