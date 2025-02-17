7d3e0e05d3b8c21f4c17ca7364f08e918de20003
NischayHR-11 <nischayhr11@gmail.com>
2025-02-17 22:46:25 +0530

    Updated Requirement file

--- render-build.sh
+++ render-build.sh
@@ -5,3 +5,6 @@ apt-get update && apt-get install -y libsm6 libxext6 libxrender-dev libgl1
 
 # Install Python dependencies
 pip install -r requirements.txt
+
+# Install Node.js dependencies
+npm install
